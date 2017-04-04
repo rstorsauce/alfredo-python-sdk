7cf13ec0a18141cd7d3ec0f8120588e7ef9b4b10
Rober Morales-Chaparro <rober.morales@gmail.com>
2017-04-04 19:01:00 +0200

    Ruote initial description object

---
+++ alfredo/descriptions.py
@@ -0,0 +1,25 @@
+ruote = {
+    'users': {
+        'me': {},
+        ':id': {}
+    },
+    'sso': {
+        'token_by_email': {},
+        'providers': {},
+    },
+    'clusters': {
+        ':id': {}
+    },
+    'queues': {
+        ':id': {}
+    },
+    'files': {
+        ':id': {}
+    },
+    'jobs': {
+        ':id': {}
+    },
+    'apps': {
+        ':id': {}
+    }
+}
